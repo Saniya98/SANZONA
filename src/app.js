f345d813a4710a1f5fc6066dd31e353100d8b609
Saniya98 <saniyafarheen.98@gmail.com>
2024-02-24 12:02:42 -0500

    Initiated routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
+import Error404Screen from "./screens/Error404Screen.js";
 import HomeScreen from "./screens/HomeScreen.js";
-const router=()=>{
-    const main= document.getElementById("main-container");
-    main.innerHTML=HomeScreen.render();
-}
-window.addEventListener('load',router)
+import ProductScreen from "./screens/ProductScreen.js";
+import { parseRequestURL } from "./screens/utils.js";
+const routes = {
+  '/': HomeScreen,
+  '/product/:id': ProductScreen,
+};
+const router = () => {
+  const request = parseRequestURL();
+  //   Using ternary operator we check if request.resource exists then `/${request.resource}`else '/'
+  const parseUrl =
+    (request.resource ? `/${request.resource}` : '/') +
+    (request.id ? '/:id':'') +
+    (request.verb ? `/${request.verb}` : '');
+
+  const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+  const main = document.getElementById("main-container"); // getting "main-container" from html file
+  main.innerHTML = screen.render();
+};
+
+window.addEventListener(
+  "load",
+  router
+); /*this line is to ensure that all the resources on the 
+webpage have been loaded before the JavaScript code is executed*/
+window.addEventListener("hashchange", router);
